3fd48c8098c437f85266b087abc607ce618db8eb
gantta <gantta@gmail.com>
2016-09-06 23:47:05 -0400

    cleanup

--- create_thumbnails/index.js
+++ create_thumbnails/index.js
@@ -64,7 +64,7 @@ exports.handler = function(event, context, callback) {
                 this.resize(width, height)
                     .toBuffer(imageType, function(err, buffer) {
                         if (err) {
-                            console.error('transform.resize error ' + error);
+                            //console.error('transform.resize error ' + err);
                             next(err);
                         } else {
                             next(null, response.ContentType, buffer);
